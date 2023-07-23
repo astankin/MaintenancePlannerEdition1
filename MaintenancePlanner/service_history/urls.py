a8b6a5f8d9f87f11c956033aa51643534336ed34
Atanas Stankin <a_stankin@abv.bg>
2023-07-23 12:12:11 +0300

    Add Department List View

--- MaintenancePlanner/service_history/urls.py
+++ MaintenancePlanner/service_history/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from MaintenancePlanner.service_history import views
-from MaintenancePlanner.service_history.views import CreateServiceReport, ReportDetailView, ReportUpdateView
+from MaintenancePlanner.service_history.views import CreateServiceReport, ReportDetailView, ReportEditView
 
 urlpatterns = [
     path('<int:pk>/', views.service_history, name='service-history'),
     path('create/', CreateServiceReport.as_view(), name='create-service-report'),
     path('report/', ReportDetailView.as_view(), name='view-report'),
-    path('report/update/<int:pk>/', ReportUpdateView.as_view(), name='update-report'),
+    path('report/edit/<int:pk>/', ReportEditView.as_view(), name='edit-report'),
 
 ]
--- MaintenancePlanner/service_history/views.py
+++ MaintenancePlanner/service_history/views.py
@@ -1,12 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView
 
 from MaintenancePlanner.equipment.models import Equipment
-from MaintenancePlanner.service_history.forms import ServiceHistoryForm, ServiceHistoryUpdateForm
+from MaintenancePlanner.service_history.forms import ServiceHistoryForm
 from MaintenancePlanner.service_history.models import ServiceHistory
 
 
@@ -37,49 +36,8 @@ class ReportDetailView(LoginRequiredMixin, DetailView):
     template_name = 'service-history.html'
 
 
-# class ReportUpdateView(LoginRequiredMixin, UpdateView):
-#     model = ServiceHistory
-#     template_name = 'update-report.html'
-#     form_class = ServiceHistoryUpdateForm
-#
-#     def dispatch(self, request, *args, **kwargs):
-#         response = super().dispatch(request, *args, **kwargs)
-#         user = self.request.user
-#         service_report = self.get_object()
-#
-#         if service_report.technician == user:
-#             return response
-#         else:
-#             raise PermissionDenied("You are not allowed to update this report.")
-#
-#     def get_success_url(self):
-#         equipment_id = self.object.equipment.id
-#         return reverse_lazy('service-history', kwargs={'pk': equipment_id})
-
-class ReportUpdateView(LoginRequiredMixin, UpdateView):
+class ReportEditView(LoginRequiredMixin, UpdateView):
     model = ServiceHistory
-    template_name = 'update-report.html'
-    form_class = ServiceHistoryUpdateForm
-
-    def __init__(self):
-        super().__init__()
-        self.has_permission = None
-
-    def dispatch(self, request, *args, **kwargs):
-        response = super().dispatch(request, *args, **kwargs)
-        user = self.request.user
-        service_report = self.get_object()
-
-        self.has_permission = service_report.technician == user
-
-        return response
-
-    def get_success_url(self):
-        equipment_id = self.object.equipment.id
-        return reverse_lazy('service-history', kwargs={'pk': equipment_id})
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['has_permission'] = self.has_permission
-        return context
-
+    template_name = 'edit-report.html'
+    fields = '__all__'
+    success_url = reverse_lazy('equipment-list')
