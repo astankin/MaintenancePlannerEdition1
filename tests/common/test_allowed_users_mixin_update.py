30c1335b79a6cd91533a5834e5218a25824ebd8a
Atanas Stankin <a_stankin@abv.bg>
2023-07-25 13:23:09 +0300

    Added permissions test

--- tests/common/test_allowed_users_mixin_update.py
+++ tests/common/test_allowed_users_mixin_update.py
@@ -3,14 +3,27 @@ from django.urls import reverse
 from django.test import TestCase
 
 from MaintenancePlanner.plant.models import Plant, Department
+from MaintenancePlanner.service_history.models import ServiceHistory
+from tests.common.test_data import create_equipment
 
 AppUser = get_user_model()
 
 
-class AllowedUsersMixinTest(TestCase):
+class AllowedUsersMixinUpdateTest(TestCase):
     def setUp(self):
         self.plant = Plant.objects.create(name='Test Plant')
         self.department = Department.objects.create(name='Test Department', plant=self.plant)
+        self.report = ServiceHistory.objects.create(
+            equipment=create_equipment(),
+            technician=AppUser.objects.create_user(
+                username='bgatsta',
+                email='bgatsta@abv.bg',
+                password='password123',
+            ),
+            problem_description='Description',
+            solution='Solution'
+        )
+        self.equipment = create_equipment()
         self.manager_user = AppUser.objects.create_user(username='manager', password='password123')
         self.manager_user.role = 'MANAGER'
         self.manager_user.save()
@@ -70,3 +83,39 @@ class AllowedUsersMixinTest(TestCase):
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, reverse('home-page'))
+
+    def test_allowed_users_can_access_report_update_view(self):
+        self.client.login(username='manager', password='password123')
+        url = reverse('edit-report', kwargs={'pk': self.report.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        self.client.login(username='supervisor', password='password123')
+        url = reverse('edit-report', kwargs={'pk': self.report.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        self.client.login(username='operator', password='password123')
+        url = reverse('edit-report', kwargs={'pk': self.report.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_allowed_users_can_access_edit_equipment_view(self):
+        self.client.login(username='manager', password='password123')
+        url = reverse('edit-equipment', kwargs={'pk': self.equipment.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        self.client.login(username='supervisor', password='password123')
+        url = reverse('edit-equipment', kwargs={'pk': self.equipment.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_disallowed_users_can_not_access_equipment_update_view(self):
+        self.client.login(username='operator', password='password123')
+        url = reverse('edit-equipment', kwargs={'pk': self.equipment.pk})
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('home-page'))
+
