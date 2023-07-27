bb1b2b8e84ced4fd9aba6ff631b827d4dc924c5c
Atanas Stankin <a_stankin@abv.bg>
2023-07-27 18:59:15 +0300

    Update

--- MaintenancePlanner/service_history/admin.py
+++ MaintenancePlanner/service_history/admin.py
@@ -6,7 +6,7 @@ from MaintenancePlanner.service_history.models import ServiceHistory
 # Register your models here.
 @admin.register(ServiceHistory)
 class ServiceHistoryAdmin(admin.ModelAdmin):
-    fields = ['created_at', 'equipment', 'technician', 'problem_description', 'solution']
+    fields = ['equipment', 'technician', 'problem_description', 'solution']
     list_display = ['created_at', 'equipment', 'technician']
     list_filter = ['created_at', 'equipment', 'technician']
     ordering = ['equipment', 'created_at']
